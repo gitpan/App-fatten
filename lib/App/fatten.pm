16ac831fc3f45e063ddd6dca1d6667643c94b398
perlancar <perlancar@gmail.com>
2014-11-21 10:10:46 +0000

    Import of PERLANCAR/App-fatten-0.18.tar.gz from CPAN.
    gitpan-cpan-distribution: App-fatten
    gitpan-cpan-version:      0.18
    gitpan-cpan-path:         PERLANCAR/App-fatten-0.18.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/App/fatten.pm
+++ lib/App/fatten.pm
@@ -1,7 +1,7 @@
 package App::fatten;
 
-our $DATE = '2014-11-20'; # DATE
-our $VERSION = '0.17'; # VERSION
+our $DATE = '2014-11-21'; # DATE
+our $VERSION = '0.18'; # VERSION
 
 use 5.010001;
 use strict;
@@ -529,7 +529,7 @@ App::fatten - Pack your dependencies onto your script file
 
 =head1 VERSION
 
-This document describes version 0.17 of App::fatten (from Perl distribution App-fatten), released on 2014-11-20.
+This document describes version 0.18 of App::fatten (from Perl distribution App-fatten), released on 2014-11-21.
 
 =head1 SYNOPSIS
 
